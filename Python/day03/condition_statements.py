c1947046198f27486378df6ffc0184ad58cd0fe9
Ruth Maina <mainaruth929@gmail.com>
2024-10-02 13:04:12 +0300

    conditin_statements

---
+++ Python/day03/condition_statements.py
@@ -0,0 +1,18 @@
+choice = input ("Choose a day of the week Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday: ")
+
+if choice == 'Monday':
+    print("NO")
+elif choice == 'Tuesday':
+    print("CLOSE ENOUGH")
+elif choice == 'Wednesday':
+    print("CORRECT")
+elif choice == 'Thursday':
+    print ("CLOSE ENOUGH")
+elif choice == 'Friday':
+    print("NO")
+elif choice == 'Saturday':
+    print("NO")
+elif choice == 'Sunday':
+    print("NO")
+else :
+    print ("Invalid choice")
